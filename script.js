99d7dc7bf490574c7207b0c8389b05fa8d0950e2
RafaelIgnacioMontes <rafaellending@gmail.com>
2023-01-24 15:03:15 -0700

    added cards and set up vanishing if wrong

--- script.js
+++ script.js
@@ -4,22 +4,63 @@ let textType = document.querySelector('input')
 
 window.keyWord = 0
 
+let boxElements = []
+
+for (let i = 1; i < 7; i++) {
+  let id = 'box' + i
+  boxElements.push(document.getElementById(id))
+}
 let secretDisplay = document.getElementById('board')
 
 let revealArray = []
 
+const setRevealArray = () => {
+  if (revealArray.length === 0) {
+    for (let i = 0; i < window.keyWord.length; i++) {
+      revealArray.push('_')
+    }
+  }
+  console.log(revealArray)
+}
+
+let incorrectGuess = 0
+
+let revealBox = () => {
+  let updateCount = 0
+  boxElements.forEach((box) => {
+    console.log(updateCount)
+    if (box.style.visibility !== 'visible' && updateCount === 0) {
+      box.style.visibility = 'visible'
+      updateCount++
+    }
+  })
+}
+
 let revealDisplay = () => {
-  secretDisplay.innerText = revealArray
+  secretDisplay.innerText = ''
+  for (let i = 0; i < revealArray.length; i++) {
+    secretDisplay.innerText = secretDisplay.innerText + ' ' + revealArray[i]
+  }
 }
 
 let letterFunction = () => {
-  for (i = 0; i < keyWord.length; i++) {
-    if (textType.value.toUpperCase() === window.keyWord[i]) {
+  setRevealArray()
+  console.log(window.keyWord)
+  let guessCorrectLetter = false
+  for (let i = 0; i < keyWord.length; i++) {
+    if (
+      textType.value.toUpperCase() === window.keyWord[i] &&
+      revealArray[i] === '_'
+    ) {
       revealArray[i] = window.keyWord[i]
-    } else if (revealArray[i] != '_') {
+      guessCorrectLetter = true
     } else {
-      revealArray[i] = '_'
+      console.log(revealArray)
     }
+  }
+  if (guessCorrectLetter === false) {
+    incorrectGuess++
+    revealBox()
   }
   revealDisplay()
 }
